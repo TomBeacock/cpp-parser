1ee03dd92c80c185760e529c96773d223d7afeac
Thomas Beacock <thomasbeacock@gmail.com>
2024-08-14 13:02:43 +0100

    Add get remaining count

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -21,16 +21,18 @@ class Parser {
   protected:
     Parser(std::string_view data);
 
-    inline Bool is_eof() const;
     inline Char get_current() const;
-    inline void move_next();
+    inline Bool is_eof() const;
     inline Bool is_equal(Char c) const;
     inline Bool in_bounds(Char min, Char max) const;
+
+    inline void move_next();
+    inline Size get_remaining() const;
 
     inline void push_save();
     inline void pop_save();
     inline void load_save(Bool pop = true);
-    inline size_t get_save_length();
+    inline Size get_save_length();
     inline std::string_view get_save_string();
 
     Bool require(Char c);
@@ -46,27 +48,42 @@ class Parser {
 
   private:
     std::string_view data;
-    size_t current_index;
-    std::stack<size_t> saved_indices;
+    Size current_index;
+    std::stack<Size> saved_indices;
 };
 }  // namespace Parsing
 
 namespace Parsing {
 inline Parser::Parser(std::string_view data) : data(data), current_index(0) {}
 
-inline Bool Parser::is_eof() const
-{
-    return this->current_index >= data.size();
-}
-
 inline Char Parser::get_current() const
 {
     return this->data[this->current_index];
 }
 
+inline Bool Parser::is_eof() const
+{
+    return this->current_index >= data.size();
+}
+
+inline Bool Parser::is_equal(Char c) const
+{
+    return get_current() == c;
+}
+
+inline Bool Parser::in_bounds(Char min, Char max) const
+{
+    return get_current() >= min && get_current() <= max;
+}
+
 inline void Parser::move_next()
 {
     this->current_index++;
+}
+
+inline Size Parser::get_remaining() const
+{
+    return this->data.size() - this->current_index;
 }
 
 inline void Parser::push_save()
@@ -81,31 +98,21 @@ inline void Parser::pop_save()
 
 inline void Parser::load_save(Bool pop)
 {
-    size_t save_index = this->saved_indices.top();
+    Size save_index = this->saved_indices.top();
     this->current_index = save_index;
     if (pop) {
         this->saved_indices.pop();
     }
 }
 
-inline size_t Parser::get_save_length()
+inline Size Parser::get_save_length()
 {
     return this->current_index - saved_indices.top();
 }
 
 inline std::string_view Parser::get_save_string()
 {
-    size_t save_index = saved_indices.top();
+    Size save_index = saved_indices.top();
     return std::string_view(&this->data[save_index], get_save_length());
-}
-
-inline Bool Parser::is_equal(Char c) const
-{
-    return get_current() == c;
-}
-
-inline Bool Parser::in_bounds(Char min, Char max) const
-{
-    return get_current() >= min && get_current() <= max;
 }
 }  // namespace Parsing
